7e46779fa08902d7e04b5e9bdf3142edc9691107
MaxouMiles-tech <66181773+MaxouMiles-tech@users.noreply.github.com>
2020-12-22 14:27:26 +0100

    test

--- migrations/Version20201217091410.php
+++ migrations/Version20201217091410.php
@@ -12,18 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201217091410 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE frorder_details (id INT AUTO_INCREMENT NOT NULL, order_details_quantity_ordered INT NOT NULL, order_details_delivery_date DATE NOT NULL, order_details_taxe_coefficient NUMERIC(4, 2) NOT NULL, order_details_delivery_adress VARCHAR(255) NOT NULL, order_details_billing_adress VARCHAR(255) DEFAULT NULL, order_details_payment VARCHAR(3) NOT NULL, order_details_send VARCHAR(3) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE frorder_details');
