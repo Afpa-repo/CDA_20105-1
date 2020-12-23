9d9e46e5e52f690c3535e6ec95d4b030cbfdf124
Boldague <gottis_thomas@hotmail.fr>
2020-12-23 14:49:51 +0100

    Fixtures TG

--- migrations/Version20201217140351.php
+++ migrations/Version20201217140351.php
@@ -20,14 +20,12 @@ final class Version20201217140351 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE frsub_category (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE frreviews ADD reviews_comments VARCHAR(255) DEFAULT NULL, ADD reviews_rate INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE frsub_category');
         $this->addSql('ALTER TABLE frreviews DROP reviews_comments, DROP reviews_rate');
     }
 }
